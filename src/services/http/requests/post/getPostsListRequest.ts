3dc79aafa38cac4a06c7f8a54e0a0cf5061ec449
ericknathan <erick.capito@hotmail.com>
2023-04-24 13:45:59 -0300

    feat: add offset and limit params to get posts list request

--- src/services/http/requests/post/getPostsListRequest.ts
+++ src/services/http/requests/post/getPostsListRequest.ts
@@ -1,5 +1,12 @@
 import { httpClient } from "../../client";
 
-export async function getPostsListRequest() {
-  return httpClient.get("/").then((response) => response.data);
+export async function getPostsListRequest({ offset = 0, limit = 10 }) {
+  return httpClient
+    .get("/", {
+      params: {
+        offset,
+        limit,
+      },
+    })
+    .then((response) => response.data);
 }
