2ef16a7e9eaf160c0df4d993e5b76589450a5e30
ericknathan <erick.capito@hotmail.com>
2023-04-24 11:54:54 -0300

    feat: use signin schema on signin page form

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,22 +1,44 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
+import { SignInSchema, signInSchema } from "@/services/validation/schemas";
+
 import { Button, TextField } from "@/components/Form";
 import { Heading } from "@/components/Text";
 
 import { Form, PageContainer } from "./styles";
 
 export function SignIn() {
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm<SignInSchema>({
+    resolver: zodResolver(signInSchema),
+  });
+
+  const handleSignIn: SubmitHandler<SignInSchema> = () => {
+    navigate("/");
+  };
+
   return (
     <PageContainer>
-      <Form action="">
+      <Form onSubmit={handleSubmit(handleSignIn)}>
         <Heading as="h1">Welcome to CodeLeap network!</Heading>
         <TextField.Fieldset>
           <TextField.Label htmlFor="username">Username</TextField.Label>
           <TextField.Input
             id="username"
-            name="username"
             placeholder="John Doe"
+            {...register("username")}
           />
         </TextField.Fieldset>
-        <Button align="end">ENTER</Button>
+        <Button align="end" disabled={!isValid}>
+          ENTER
+        </Button>
       </Form>
     </PageContainer>
   );
