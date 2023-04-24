6cea83f580d049787cd74d41ce230876e6e95265
ericknathan <erick.capito@hotmail.com>
2023-04-24 11:55:32 -0300

    feat: dispatch signIn action on signin form

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,10 @@
+import { RootState } from "@/redux/store";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
+import { signIn } from "@/redux/slices/authSlice";
 import { SignInSchema, signInSchema } from "@/services/validation/schemas";
 
 import { Button, TextField } from "@/components/Form";
@@ -12,15 +15,22 @@ import { Form, PageContainer } from "./styles";
 export function SignIn() {
   const navigate = useNavigate();
 
+  const auth = useSelector((state: RootState) => state.auth);
+  const dispatch = useDispatch();
+
   const {
     register,
     handleSubmit,
     formState: { isValid },
   } = useForm<SignInSchema>({
     resolver: zodResolver(signInSchema),
+    defaultValues: {
+      username: auth.username,
+    },
   });
 
-  const handleSignIn: SubmitHandler<SignInSchema> = () => {
+  const handleSignIn: SubmitHandler<SignInSchema> = ({ username }) => {
+    dispatch(signIn(username));
     navigate("/");
   };
 
